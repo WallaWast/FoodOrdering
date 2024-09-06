3d1fbca2d64c1562c9307aae3e672902de05f1c2
Wallace Torres <Wallace.DaSilvaTorres@campus-support.ca>
2024-09-06 11:27:21 -0700

    Product list improvements

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -14,6 +14,7 @@ const ProductListItem = ({ product }: ProductListItemProps) => {
       <Image
         source={{ uri: product.image || defaultPizzaImage }}
         style={styles.image}
+        resizeMode="contain"
       />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
@@ -24,7 +25,13 @@ const ProductListItem = ({ product }: ProductListItemProps) => {
 export default ProductListItem;
 
 const styles = StyleSheet.create({
-  container: { backgroundColor: "white", padding: 10, borderRadius: 20 },
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
+    flex: 1,
+    maxWidth: "50%",
+  },
   image: { width: "100%", aspectRatio: 1 },
   title: { fontSize: 20, fontWeight: "600", marginVertical: 10 },
   price: { color: Colors.light.tint, fontWeight: "bold" },
