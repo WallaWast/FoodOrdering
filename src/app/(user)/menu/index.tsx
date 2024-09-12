a004f3ac4893641f8e39df5d5a2d9a15a9974df8
Wallace Torres <Wallace.DaSilvaTorres@campus-support.ca>
2024-09-12 10:15:16 -0700

    query products

--- src/app/(user)/menu/index.tsx
+++ src/app/(user)/menu/index.tsx
@@ -1,8 +1,33 @@
-import { FlatList } from "react-native";
-import products from "@assets/data/products";
+import { ActivityIndicator, FlatList, Text } from "react-native";
 import ProductListItem from "@components/ProductListItem";
+import { supabase } from "@/src/lib/supabase";
+import { useQuery } from "@tanstack/react-query";
 
 export default function MenuScreen() {
+  const {
+    data: products,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: ["products"],
+    queryFn: async () => {
+      const { data, error } = await supabase.from("products").select("*");
+      if (error) {
+        throw new Error(error.message);
+      }
+
+      return data;
+    },
+  });
+
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return <Text>Failed to fetch products</Text>;
+  }
+
   return (
     <FlatList
       data={products}
